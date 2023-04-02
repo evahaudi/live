256d356b1bb1337ddad063034f8f0467cc057367
Everlyne Akinyi <evahaudi2354@gmail.com>
2023-04-03 00:01:44 +0300

    updated

--- src/scenes/vat/index.jsx
+++ src/scenes/vat/index.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import { Box,Typography,useTheme} from "@mui/material";
+import { DataGrid, GridToolbar  } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
 
 const Graph = (props) => {
   const [owners, setOwners] = useState([]);
   const [falseImports, setFalseImports] = useState([]);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
   useEffect(() => {
     fetch(`http://10.153.1.85:8000/fraud_app/api/v1/owner_and_false_imports/${props.pinNo}/`)
@@ -16,6 +21,8 @@ const Graph = (props) => {
       })
       .catch(error => console.error(error));
   }, [props.pinNo]);
+  console.log(owners);
+  console.log(falseImports);
 
   const ownersColumns = [
     { field: "tax_payer_name", headerName: "Name", width: 200 },
@@ -33,12 +40,137 @@ const Graph = (props) => {
   ];
 
   return (
-    <div>
-      <h2>Owners</h2>
-      <DataGrid rows={owners} columns={ownersColumns} pageSize={10} />
-      <h2>False Imports</h2>
-      <DataGrid rows={falseImports} columns={falseImportsColumns} pageSize={10} />
-    </div>
+    // <div>
+    //   <h2>Owners</h2>
+    //   <DataGrid rows={owners} columns={ownersColumns} pageSize={10} />
+    //   <h2>False Imports</h2>
+    //   <DataGrid rows={falseImports} columns={falseImportsColumns} pageSize={10} />
+    // </div>
+
+    <Box m="20px">
+    <Header
+      title="Imports & owners"
+      subtitle="False imports  and owners data"
+    />
+    <Box
+      m="40px 0 0 0"
+      height="72vh"
+      width="1200px"
+      
+      sx={{
+        "& .MuiDataGrid-root": {
+          border: "none",
+        },
+        "& .MuiDataGrid-cell": {
+          borderBottom: "0.5px solid",
+          fontFamily: 'Roboto, sans-serif',
+          fontSize:14,
+        },
+        "& .name-column--cell": {
+          color: colors.white[100],
+          fontFamily: 'Roboto, sans-serif',
+          fontSize:14,
+        },
+        "& .MuiDataGrid-columnHeaders": {
+          backgroundColor: colors.black[300],
+          color: colors.white[100],
+          borderBottom: "none",
+          fontFamily: 'Roboto, sans-serif',
+          fontSize:14,
+        },
+        "& .MuiDataGrid-virtualScroller": {
+          backgroundColor: colors.primary[400],
+        },
+        "& .MuiDataGrid-footerContainer": {
+          borderTop: "none",
+          backgroundColor: colors.redAccent[500],
+       
+        },
+        "& .MuiCheckbox-root": {
+          color: `${colors.redAccent[400]} !important`,
+        },
+        "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+          color: `${colors.grey[100]} !important`,
+        },
+      }}
+    > 
+
+ <DataGrid
+  rows={falseImports}
+  columns={falseImportsColumns}
+  checkboxSelection
+  pageSize={20}
+  rowsPerPageOptions={[20]}
+  components={{
+    Toolbar: GridToolbar,
+  }}
+
+/>
+
+</Box>  
+<Box
+      m="40px 0 0 0"
+      height="72vh"
+      width="1200px"
+      
+      sx={{
+        "& .MuiDataGrid-root": {
+          border: "none",
+        },
+        "& .MuiDataGrid-cell": {
+          borderBottom: "0.5px solid",
+          fontFamily: 'Roboto, sans-serif',
+          fontSize:14,
+        },
+        "& .name-column--cell": {
+          color: colors.white[100],
+          fontFamily: 'Roboto, sans-serif',
+          fontSize:14,
+        },
+        "& .MuiDataGrid-columnHeaders": {
+          backgroundColor: colors.black[300],
+          color: colors.white[100],
+          borderBottom: "none",
+          fontFamily: 'Roboto, sans-serif',
+          fontSize:14,
+        },
+        "& .MuiDataGrid-virtualScroller": {
+          backgroundColor: colors.primary[400],
+        },
+        "& .MuiDataGrid-footerContainer": {
+          borderTop: "none",
+          backgroundColor: colors.redAccent[500],
+       
+        },
+        "& .MuiCheckbox-root": {
+          color: `${colors.redAccent[400]} !important`,
+        },
+        "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+          color: `${colors.grey[100]} !important`,
+        },
+      }}
+    > 
+
+<DataGrid
+  rows={owners}
+  columns={ownersColumns}
+  rowKey="id"
+  checkboxSelection
+  pageSize={20}
+  rowsPerPageOptions={[20]}
+  components={{
+    Toolbar: GridToolbar,
+  }}
+  
+/>
+
+</Box>  
+    <Box textAlign="center"  >
+        <Typography variant="h5" color={colors.grey[100]}>
+          @2023KRA Copyrights.
+        </Typography>   
+      </Box>  
+  </Box>
   );
 }
 export default Graph;
