94f8bddf5b52d86bb5cf8bef1011c4a8f195e613
everlyne akinyi <evahaudi2354@gmail.com>
2023-04-22 02:18:15 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { useState } from "react";
-import Iframe from 'react-iframe';
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
-// import Dashboard from "./scenes/dashboard";
+import Dashboard from "./scenes/dashboard";
 import Customer from "./scenes/tax";
 import Graph from "./scenes/vat";
-
 import Contacts from "./scenes/contacts";
-import Employee from "./scenes/table"; 
-
-
+import Employee from "./scenes/table";
+import Login from "./scenes/login";
+import Signup from "./scenes/signup";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 
@@ -28,28 +26,13 @@ function App() {
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              {/* <Route path="/" element={<iframe src='http://127.0.0.1:5500/index.html' height="500" width="1250" title='popot' padding ="40px" />} />  */}
-              <Route
-  path="/"
-  element={
-    <div style={{ padding: "40px", marginTop:"0px"}}>
-      <Iframe
-        src="http://127.0.0.1:5500/index.html"
-        height="700"
-        width="1250"
-        title="popoto"
-        style={{ border: "none", borderRadius: "5px" }} // add custom styles
-      />
-    </div>
-  }
-/>
+              <Route path="/" element={Dashboard}/>
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/table" element={<Employee />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
               <Route path="/vat" element={<Customer />} />
               <Route path="/tax" element={<Graph />} />
-              
-          
-         
             </Routes>
           </main>
         </div>
---
+++ src/scenes/table/signu.js
@@ -0,0 +1,164 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+    },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '1rem',
+        borderRadius: '0.5rem',
+        backgroundColor: '#f2f2f2',
+        boxShadow: '0 0.25rem 0.5rem rgba(0, 0, 0, 0.1)',
+        maxWidth: '30%',
+        margin: '1rem',
+        [theme.breakpoints.down('sm')]: {
+            maxWidth: '80%',
+        },
+    },
+    input: {
+        margin: '0.2rem 0',
+    },
+    button: {
+        margin: '1rem 0',
+        backgroundColor: '#ff0000',
+        color: '#fff',
+        '&:hover': {
+            backgroundColor: '#cc0000',
+        },
+    },
+    logo: {
+        marginBottom: '1rem',
+        maxWidth: '100%',
+    },
+}));
+
+const Signup = () => {
+    const classes = useStyles();
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [fullnameError, setFullnameError] = useState(false);
+
+    const validateEmail = (email) => {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    const validatePassword = (password) => {
+        return password.length >= 4;
+    }
+
+    const validatefullName = (fullName) => {
+        return fullName.length >= 8;
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!email) {
+            setEmailError('Email is required');
+            return;
+        } else if (!validateEmail(email)) {
+            setEmailError('Please enter a valid email address');
+            return;
+        } else {
+            setEmailError('');
+        }
+
+        if (!password) {
+            setPasswordError('Password is required');
+            return;
+        } else if (!validatePassword(password)) {
+            setPasswordError('Password should be at least 4 characters long');
+            return;
+        } else {
+            setPasswordError('');
+        }
+
+        if (!fullName) {
+            setFullnameError('fullName is required');
+            return;
+        } else if (!validatefullName(password)) {
+            setFullnameError('fullName should be at least 8 characters long');
+            return;
+        } else {
+            setFullnameError('');
+        }
+        console.log(`Full Name: ${fullName} Email: ${email} Password: ${password}`);
+    };
+
+    return (
+        <div className={classes.root}>
+            <form onSubmit={handleSubmit} className={classes.form}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <img src="https://lenvica.com/wp-content/uploads/2021/05/KRA-P9Form.png" alt="logo" className={classes.logo} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Full Name"
+                            variant="outlined"
+                            fullWidth
+                            className={classes.input}
+                            value={fullName}
+                            onChange={(e) => setFullName(e.target.value)}
+                            error={Boolean(emailError)}
+                            helperText={emailError}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Email"
+                            variant="outlined"
+                            fullWidth
+                            className={classes.input}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            error={Boolean(passwordError)}
+                            helperText={passwordError}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Password"
+                            variant="outlined"
+                            fullWidth
+                            className={classes.input}
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            error={Boolean(fullnameError)}
+                            helperText={fullnameError}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button type="submit" variant="contained" className={classes.button}>
+                            Sign up
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button variant="text" color="primary">Forgot password?</Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Link to="/Login" style={{ color: '#ff6666' }}>Already registered click to? Login</Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </div>
+    );
+};
+
+export default Signup;
